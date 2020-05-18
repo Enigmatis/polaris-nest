0b61ac97fd8dcfc68ded5507b2d4c8d96a4b1dcc
Arik F <furmanmail@gmail.com>
2020-05-18 14:01:51 +0300

    fix: added travis

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject, Redirect, Req } from "@nestjs/common";
+import { Controller, Get, Redirect, Req } from "@nestjs/common";
 import { RoutesService } from "./routes.service";
 
 @Controller()
