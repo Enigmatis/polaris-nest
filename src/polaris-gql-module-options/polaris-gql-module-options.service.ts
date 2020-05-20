a8ec348bd2ae5ec5d615942e97554264fcfad7d5
chenshoo <chenshooker@gmail.com>
2020-05-20 14:17:19 +0300

    copied graphqlmodule

--- src/polaris-gql-module-options/polaris-gql-module-options.service.ts
+++ src/polaris-gql-module-options/polaris-gql-module-options.service.ts
@@ -15,7 +15,6 @@ import { PolarisGraphQLLogger } from "@enigmatis/polaris-graphql-logger";
 import { SubscriptionServerOptions } from "apollo-server-core/src/types";
 import { PlaygroundConfig } from "apollo-server";
 import { GraphQLSchema } from "graphql";
-import { PolarisLoggerService } from "../polaris-logger/polaris-logger.service";
 import { PolarisServerConfigService } from "../polaris-server-config/polaris-server-config.service";
 import { PolarisServerConfig } from "@enigmatis/polaris-core/dist/src/config/polaris-server-config";
 import { Injectable } from "@nestjs/common";
